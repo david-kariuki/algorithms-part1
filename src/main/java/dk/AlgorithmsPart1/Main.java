8a7ed40b9b091db00d4131f731919ef9fa4fb056
David Kariuki <workplace.davidkariuki@gmail.com>
2022-07-25 20:22:16 +0300

    Union find

--- src/main/java/dk/AlgorithmsPart1/Main.java
+++ src/main/java/dk/AlgorithmsPart1/Main.java
@@ -1,7 +1,26 @@
 package dk.AlgorithmsPart1;
 
+
+import dk.AlgorithmsPart1.utilities.algs4.StdIn;
+import dk.AlgorithmsPart1.utilities.algs4.StdOut;
+
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello world!");
+
+		int N = StdIn.readInt();
+
+		UnionFind unionFind = new UnionFind(N);
+
+		while (!StdIn.isEmpty()) {
+			int p = StdIn.readInt();
+			int q = StdIn.readInt();
+
+			if (!unionFind.connected(p, q)) {
+				unionFind.union(p, q);
+				StdOut.println(p + " " + q);
+			}
+		}
+
+
 	}
 }
